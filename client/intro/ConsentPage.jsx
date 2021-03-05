ca049cbba9e8d256c3f36d0a4369f72dfe380aae
Brian Chivers <bchivers@stanford.edu>
2021-03-05 11:49:24 -0800

    edit consent page

--- client/intro/ConsentPage.jsx
+++ client/intro/ConsentPage.jsx
@@ -23,34 +23,30 @@ export default class Consent extends React.Component {
         <div className="consent">
           <h1> Consent Form </h1>
           <p>
-          This task is part of a Stanford University research study looking at the ability of
-          people to understand the visual preferences of animals. Your decision to participate is
-          entirely voluntary. The whole task should take around 15 minutes and you will be
-          compensated for your time and good performance, as detailed in the instructions.
+          This task is part of a Stanford University research study looking at the ability of people to understand the visual preferences of animals. 
+          Your decision to participate is entirely voluntary. You will play three games and the whole task should take around 15 minutes. 
+          You will receive $2.00 for completing the task and $0.001 per point earned in each game. 
+          The points per turn range from 0 to 100 and there are 25 turns per game so an average of 50 points per turn in would yield an additional $1.25 and the maximum bonus per game is $2.50. 
+          You may stop participating in the study at any time and you may choose not to answer questions.
           <br/>
           <br/>
 
-          There are no anticipated risks involved in doing this task. The only information
-          we gather are your Prolific ID, your behavior in the tasks. Your data points will
-          become one of hundreds we collect. The aggregated results of our research may be
-          presented at scientific meetings or published in scientific journals.
-          <br/>
-          <br/>
-
-
-          If you have questions or concerns, please contact the study director Jon Atwell
-          at atwell@stanford.edu. If you have any complaints, concerns or general
-          questions about the study or your rights as a participant, please contact the
-          Stanford Institutional Review Board (IRB) at 650-723-2480, email at IRB2-
-          Manager@lists.stanford.edu or toll free at 1-866-680-2906. You can also write to
-          the Stanford IRB, Stanford University, 1705 El Camino Real, Palo Alto, CA
-          94306.
+          There are no anticipated risks involved in doing this task. The only information we gather are your Prolific ID and your behavior in the tasks. 
+          Your data points will become one of hundreds we collect. The aggregated results of our research may be presented at scientific meetings or published in scientific journals. 
+          We cannot and do not guarantee or promise that you will receive any benefits from this study.
           <br/>
           <br/>
 
 
-          Selecting "AGREE" below indicates that you are at least 18 years of age, and
-          agree to participate voluntary. If you select DISAGREE, you will exit
+          If you have questions or concerns, please contact the study director Jon Atwell at atwell@stanford.edu. 
+          If you have any complaints, concerns or general questions about the study or your rights as a participant, please contact the Stanford Institutional Review Board (IRB) at irbnonmed@stanford.edu. 
+          Please save or print a copy of this information for your records.
+          <br/>
+          <br/>
+
+
+          Selecting "AGREE" below indicates that you are at least 18 years of age, and agree to participate voluntary. 
+          If you select DISAGREE, you will exit.
           </p>
           <br />
           <ConsentButton text="AGREE" />
